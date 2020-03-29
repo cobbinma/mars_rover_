f7ae31b8be2832fa1b4bc9b6d4f0da5420a91ab8
matthewcobbing <matthewc@nandos.co.uk>
2020-03-29 14:56:19 +0100

    add rover turn logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,59 @@
+#[derive(Debug, PartialEq)]
 pub struct Rover {
+    bearing: Bearing,
     coordinates: Coordinates,
+}
+
+impl Rover {
+    fn new(x_coordinate: i64, y_coordinate:i64, bearing: Bearing) -> Rover {
+        Rover{ bearing, coordinates: Coordinates{x_coordinate, y_coordinate}}
+    }
+
+    fn get_coordinates(&self) -> &Coordinates {
+        &self.coordinates
+    }
+
+    fn move_north(&mut self) {
+        self.coordinates.north();
+    }
+
+    fn move_south(&mut self) {
+        self.coordinates.south();
+    }
+
+    fn move_east(&mut self) {
+        self.coordinates.east();
+    }
+
+    fn move_west(&mut self) {
+        self.coordinates.west();
+    }
+
+    fn turn_right(&mut self) {
+        match self.bearing {
+            Bearing::North => self.bearing = Bearing::East,
+            Bearing::East => self.bearing = Bearing::South,
+            Bearing::South => self.bearing = Bearing::West,
+            Bearing::West => self.bearing = Bearing::North,
+        }
+    }
+
+    fn turn_left(&mut self) {
+        match self.bearing {
+            Bearing::North => self.bearing = Bearing::West,
+            Bearing::East => self.bearing = Bearing::North,
+            Bearing::South => self.bearing = Bearing::East,
+            Bearing::West => self.bearing = Bearing::South,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Bearing {
+    North,
+    East,
+    South,
+    West,
 }
 
 #[derive(Debug, PartialEq)]
@@ -26,81 +80,79 @@ impl Coordinates {
     }
 }
 
-impl Rover {
-    fn new(x_coordinate: i64, y_coordinate:i64) -> Rover {
-        Rover{coordinates: Coordinates{x_coordinate, y_coordinate}}
-    }
-
-    fn get_coordinates(&self) -> &Coordinates {
-        &self.coordinates
-    }
-
-    fn move_north(&mut self) {
-        self.coordinates.north();
-    }
-
-    fn move_south(&mut self) {
-        self.coordinates.south();
-    }
-
-    fn move_east(&mut self) {
-        self.coordinates.east();
-    }
-
-    fn move_west(&mut self) {
-        self.coordinates.west();
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn move_north() {
-        let mut rover = Rover::new(0, 0);
+        let mut rover = Rover::new(0, 0, Bearing::North);
 
         rover.move_north();
 
         assert_eq!(
-            Coordinates{ x_coordinate: 0, y_coordinate: 1 },
-            *rover.get_coordinates()
+            Rover::new(0, 1, Bearing::North),
+            rover
         );
     }
 
     #[test]
     fn move_south() {
-        let mut rover = Rover::new(0, 1);
+        let mut rover = Rover::new(0, 1, Bearing::North);
 
         rover.move_south();
 
         assert_eq!(
-            Coordinates{ x_coordinate: 0, y_coordinate: 0 },
-            *rover.get_coordinates()
+            Rover::new(0, 0, Bearing::North),
+            rover
         );
     }
 
     #[test]
     fn move_east() {
-        let mut rover = Rover::new(0, 0);
+        let mut rover = Rover::new(0, 0, Bearing::North);
 
         rover.move_east();
 
         assert_eq!(
-            Coordinates{ x_coordinate: 1, y_coordinate: 0 },
-            *rover.get_coordinates()
+            Rover::new(1, 0, Bearing::North),
+            rover
         );
     }
 
     #[test]
     fn move_west() {
-        let mut rover = Rover::new(1, 0);
+        let mut rover = Rover::new(1, 0, Bearing::North);
 
         rover.move_west();
 
         assert_eq!(
-            Coordinates{ x_coordinate: 0, y_coordinate: 0 },
-            *rover.get_coordinates()
+            Rover::new(0, 0, Bearing::North),
+            rover
+        );
+    }
+
+    #[test]
+    fn turn_right_from_north() {
+        let mut rover = Rover::new(0, 0, Bearing::North);
+
+        rover.turn_right();
+
+        assert_eq!(
+            Rover::new(0, 0, Bearing::East),
+            rover
+        );
+    }
+
+    #[test]
+    fn turn_left_from_north() {
+        let mut rover = Rover::new(0, 0, Bearing::North);
+
+        rover.turn_left();
+
+        assert_eq!(
+            Rover::new(0, 0, Bearing::West),
+            rover
         );
     }
 }
