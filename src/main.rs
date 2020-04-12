3600a252d881806e146b6b54965e7bd3b3498cfb
matthewcobbing <matthewc@nandos.co.uk>
2020-04-12 14:20:00 +0100

    return error codes from main errors

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let config = match Config::new(&args) {
         Ok(val) => val,
-        Err(e) => panic!("could not create config : {}", e),
+        Err(e) => {
+            eprintln!("could not create config : {}", e);
+            std::process::exit(1)
+        },
     };
 
     let rovers = match mars_rover::deploy_rovers(config) {
         Ok(val) => val,
-        Err(e) => panic!("could not deploy rovers : {}", e),
+        Err(e) => {
+            eprintln!("could not deploy rovers : {}", e);
+            std::process::exit(1)
+        }
     };
 
     for rover in rovers {
